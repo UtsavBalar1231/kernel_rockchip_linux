5bdbbb55774f847a1f5fde1bee3a4b65115a281c
Jari Hämäläinen <nuumiofi@gmail.com>
2020-07-28 12:36:41 +0300

    drm: panfrost: Silence Panfrost gem shrinker logging
    Even though ratelimited panfrost gem shrinker messages are a bit
    annoying when keeping an eye for unexpected things in kernel logs.
    
    NOTE: This is just my personal preference.
    Change-Id: I3838afef9a03abf22704a00730598c65c2ab3a64
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/gpu/drm/panfrost/panfrost_gem_shrinker.c
+++ drivers/gpu/drm/panfrost/panfrost_gem_shrinker.c
@@ -83,9 +83,6 @@ panfrost_gem_shrinker_scan(struct shrinker *shrinker, struct shrink_control *sc)
 
 	mutex_unlock(&pfdev->shrinker_lock);
 
-	if (freed > 0)
-		pr_info_ratelimited("Purging %lu bytes\n", freed << PAGE_SHIFT);
-
 	return freed;
 }
 
