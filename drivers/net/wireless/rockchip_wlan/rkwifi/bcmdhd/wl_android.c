f564679994e1c876009ffc632cfd798ce689baef
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-05-24 15:35:08 +0530

    wireless: rkwifi: wl_android: Fix an unrequired comparision in wl_handle_private_cmd
    This fixes following errors:
    ../drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/wl_android.c: In function 'wl_handle_private_cmd':
    ../drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/wl_android.c:4910:62: warning: the comparison will always evaluate as 'true' for the pointer operand in 'rev_info_delim + 1' must not be NULL [-Waddress]
    error, forbidden warning:wl_android.c:4910
     4910 |                         strlen(CMD_COUNTRY_DELIMITER)) == 0) &&
          |                                                              ^~
    
    Change-Id: I5d197bf634e2f77958fc2bc7f6eef7be11d2e6a7
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/wl_android.c
+++ drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/wl_android.c
@@ -2,13 +2,13 @@
  * Linux cfg80211 driver - Android related functions
  *
  * Copyright (C) 1999-2017, Broadcom Corporation
- * 
+ *
  *      Unless you and Broadcom execute a separate written software license
  * agreement governing use of this software, this software is licensed to you
  * under the terms of the GNU General Public License version 2 (the "GPL"),
  * available at http://www.broadcom.com/licenses/GPLv2.php, with the
  * following added to such license:
- * 
+ *
  *      As a special exception, the copyright holders of this software give you
  * permission to link this software with independent modules, and to copy and
  * distribute the resulting executable under terms of your choice, provided that
@@ -16,7 +16,7 @@
  * the license of that module.  An independent module is a module which is not
  * derived from this software.  The special exception does not apply to any
  * modifications of the software.
- * 
+ *
  *      Notwithstanding the above, under no circumstances may you combine this
  * software in any way with any other Broadcom software provided under a license
  * other than the GPL, without Broadcom's express prior written consent.
@@ -4907,8 +4907,7 @@ wl_handle_private_cmd(struct net_device *net, char *command, u32 cmd_len)
 #endif /* DHD_BLOB_EXISTENCE_CHECK */
 		if ((rev_info_delim) &&
 			(strnicmp(rev_info_delim, CMD_COUNTRY_DELIMITER,
-			strlen(CMD_COUNTRY_DELIMITER)) == 0) &&
-			(rev_info_delim + 1)) {
+			strlen(CMD_COUNTRY_DELIMITER)) == 0)) {
 			revinfo  = bcm_atoi(rev_info_delim + 1);
 		}
 		bytes_written = wldev_set_country(net, country_code, true, true, revinfo);
