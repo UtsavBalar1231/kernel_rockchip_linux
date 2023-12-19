72ada4a44f8cdaf80f1035c62afa2ad2e56496ee
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-12-19 18:58:01 +0530

    net: rockchip_wlan: rtl8822cs: Fix missing member current_bss in struct wireless_dev
    Follow the changes produced in commit: 7b0a0e3: ("wifi: cfg80211: do some rework towards MLO link APIs")
    
    This fixes following compilation error:
    ../drivers/net/wireless/rockchip_wlan/rtl8822cs/os_dep/linux/ioctl_cfg80211.c:11116:12: error: no member named 'current_bss' in 'struct wireless_dev'
     11116 |         if (wdev->current_bss) {
           |             ~~~~  ^
    
    Change-Id: I99bdb9d84603a29436e632628eb6949b46907738
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/net/wireless/rockchip_wlan/rtl8822cs/os_dep/linux/ioctl_cfg80211.c
+++ drivers/net/wireless/rockchip_wlan/rtl8822cs/os_dep/linux/ioctl_cfg80211.c
@@ -11146,7 +11146,11 @@ void rtw_wdev_unregister(struct wireless_dev *wdev)
 	rtw_cfg80211_indicate_scan_done(adapter, _TRUE);
 
 	#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 11, 0)) || defined(COMPAT_KERNEL_RELEASE)
+	#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 0))
+	if (wdev->links[0].client.current_bss) {
+	#else
 	if (wdev->current_bss) {
+	#endif
 		RTW_INFO(FUNC_ADPT_FMT" clear current_bss by cfg80211_disconnected\n", FUNC_ADPT_ARG(adapter));
 		rtw_cfg80211_indicate_disconnect(adapter, 0, 1);
 	}
