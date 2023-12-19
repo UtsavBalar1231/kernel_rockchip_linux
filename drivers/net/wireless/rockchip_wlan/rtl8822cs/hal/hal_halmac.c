7b708d97c1f9d7448111f94eb69f6c0a0f0cb90c
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-12-19 19:05:19 +0530

    net: rockchip_wlan: rtl8822cs: Fix implicit-fallthrough errors
    This fixes following warnings when compiling using clang-18:
    ../drivers/net/wireless/rockchip_wlan/rtl8822cs/hal/rtl8822c/rtl8822c_phy.c:1386:3: warning: unannotated fall-through between switch labels [-Wimplicit-fallthrough]
     1386 |                 case CHANNEL_WIDTH_40:
          |                 ^
    ../drivers/net/wireless/rockchip_wlan/rtl8822cs/hal/rtl8822c/rtl8822c_phy.c:1386:3: note: insert '__attribute__((fallthrough));' to silence this warning
     1386 |                 case CHANNEL_WIDTH_40:
          |                 ^
          |                 __attribute__((fallthrough));
    ../drivers/net/wireless/rockchip_wlan/rtl8822cs/hal/rtl8822c/rtl8822c_phy.c:1386:3: note: insert 'break;' to avoid fall-through
     1386 |                 case CHANNEL_WIDTH_40:
          |                 ^
          |                 break;
    ../drivers/net/wireless/rockchip_wlan/rtl8822cs/hal/rtl8822c/rtl8822c_phy.c:1392:3: warning: unannotated fall-through between switch labels [-Wimplicit-fallthrough]
     1392 |                 case CHANNEL_WIDTH_20:
          |                 ^
    ../drivers/net/wireless/rockchip_wlan/rtl8822cs/hal/rtl8822c/rtl8822c_phy.c:1392:3: note: insert '__attribute__((fallthrough));' to silence this warning
     1392 |                 case CHANNEL_WIDTH_20:
          |                 ^
          |                 __attribute__((fallthrough));
    ../drivers/net/wireless/rockchip_wlan/rtl8822cs/hal/rtl8822c/rtl8822c_phy.c:1392:3: note: insert 'break;' to avoid fall-through
     1392 |                 case CHANNEL_WIDTH_20:
          |                 ^
          |                 break;
    
    ../drivers/net/wireless/rockchip_wlan/rtl8822cs/hal/hal_halmac.c:3139:2: warning: unannotated fall-through between switch labels [-Wimplicit-fallthrough]
     3139 |         default:
          |         ^
    ../drivers/net/wireless/rockchip_wlan/rtl8822cs/hal/hal_halmac.c:3139:2: note: insert '__attribute__((fallthrough));' to silence this warning
     3139 |         default:
          |         ^
          |         __attribute__((fallthrough));
    ../drivers/net/wireless/rockchip_wlan/rtl8822cs/hal/hal_halmac.c:3139:2: note: insert 'break;' to avoid fall-through
     3139 |         default:
          |         ^
          |         break;
    
    Change-Id: I51d57b07e1a60bb39cb76cfcd71df45bf037332d
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/net/wireless/rockchip_wlan/rtl8822cs/hal/hal_halmac.c
+++ drivers/net/wireless/rockchip_wlan/rtl8822cs/hal/hal_halmac.c
@@ -3135,7 +3135,7 @@ static int _send_general_info(struct dvobj_priv *d)
 	case HALMAC_RET_NO_DLFW:
 		RTW_WARN("%s: halmac_send_general_info() fail because fw not dl!\n",
 			 __FUNCTION__);
-		/* fall through */
+		fallthrough;
 	default:
 		return -1;
 	}
